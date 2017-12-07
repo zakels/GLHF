518051ee7cdd49b90afa7ac2f70da9ba2a895d6c
Youshin Kim <kim1415@purdue.edu>
2017-12-07 01:06:29 -0500

    updated

--- myapp/models.py
+++ myapp/models.py
@@ -2,27 +2,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
-<<<<<<< HEAD
 from jsonfield import JSONField
-=======
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
->>>>>>> f37822d68501b265bc5c8fb7236337b1eeb09248
 
 # api_form = "?api_key="
 # api_key = api_form + "RGAPI-cc796626-90e8-4f70-ac34-f9e41e7a40ca"
 # Create your models here.
 
-<<<<<<< HEAD
-class Book(models.Model):
-    isbn = models.BigIntegerField(primary_key=True)
-    title = models.CharField(max_length=128)
-    memo = JSONField(default={}, dump_kwargs={'ensure_ascii': False})
-
-    def __str__(self):
-        return self.title
-=======
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     in_game_id = models.CharField(max_length=30, blank=False, unique=False)
@@ -40,4 +28,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
->>>>>>> f37822d68501b265bc5c8fb7236337b1eeb09248
